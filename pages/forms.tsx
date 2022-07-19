e74c6d2c92439d7709c61d020c5e6ac4c93100fc
장성민 <aristatait@gmail.com>
2022-07-19 11:54:23 +0900

    7.4 validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldErrors, FieldErrorsImpl, useForm } from "react-hook-form";
 
 /**
  * 일반적인 폼 =>
  * 1. 인풋마다 스테이트를 가진다
  * 2. 인풋마다 이벤트 리스너를 정의해야 한다
  * 3. 서브밋을 할 때 밸리데이션을 인풋마다 구현해야 한다
- * 
+ *
  * react-hook-form =>
  * 1. 폼보다 적은 코드로 같은 기능을 구현할 수 있다
  * 2. 인풋에 적용하기 쉽다
- * 3. 이벤트를 신경쓰지 않아도 된다 
+ * 3. 이벤트를 신경쓰지 않아도 된다
+ * 4. 밸리데이션을 쉽게 구현할 수 있다
  */
-// 더 나은 밸리데이션
 // 더 나은 에러
 // 인풋 컨트롤 유지
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
   // const [username, setUsername] = useState("");
@@ -42,13 +48,36 @@ export default function Forms() {
   //   event.preventDefault();
   //   console.log(username, email, password);
   // }
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("im valid!!!");
+  };
+  const onInvalid = (errors: FieldErrorsImpl) => {
+    console.log(errors);
+  };
   return (
-    <form>
-      <input type={"text"} placeholder="Username" {...register("username")} />
-      <input type={"email"} placeholder="Email" {...register("email")} />
-      <input type={"password"} placeholder="password" {...register("password")} />
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        type={"text"}
+        placeholder="Username"
+        {...register("username", {
+          required: "The username is required.",
+          minLength: {
+            value: 5,
+            message: "The username should be longer than 5 chars.",
+          },
+        })}
+      />
+      <input
+        type={"email"}
+        placeholder="Email"
+        {...register("email", { required: "The email is required." })}
+      />
+      <input
+        type={"password"}
+        placeholder="password"
+        {...register("password", { required: "The password is required." })}
+      />
       <input type={"submit"} value="Create Account" />
     </form>
   );

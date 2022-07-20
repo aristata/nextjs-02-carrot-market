841db7c9f20a3aacce8ee84a6ac0d19c3071e7e4
장성민 <aristatait@gmail.com>
2022-07-20 14:09:26 +0900

    Refactor: 중복코드 제거
    upsert 기능을 사용하여 조회 기능을 생략하고, 동적 조건식을 변수를 사용하여 구현함으로써 코드를 간략화 하였다

--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -4,45 +4,17 @@ import client from "@libs/backend/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  let user;
-
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone
-      }
-    });
-    if (!user) {
-      console.log("유저를 찾을 수 없습니다. 새로 생성합니다.");
-      user = await client.user.create({
-        data: {
-          phone: +phone,
-          name: "Anonymous"
-        }
-      });
-    } else {
-      console.log("유저를 찾았습니다.");
-    }
-  }
-
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email: email
-      }
-    });
-    if (!user) {
-      console.log("유저를 찾을 수 없습니다. 새로 생성합니다.");
-      user = await client.user.create({
-        data: {
-          email: email,
-          name: "Anonymous"
-        }
-      });
-    } else {
-      console.log("유저를 찾았습니다.");
-    }
-  }
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload
+    },
+    create: {
+      name: "Anonymous",
+      ...payload
+    },
+    update: {}
+  });
 
   console.log(user);
   return res.status(200).end();

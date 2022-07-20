db73fb06547573744e9d3abadd83d78ee20ddfc4
장성민 <aristatait@gmail.com>
2022-07-20 17:55:00 +0900

    Feat: 이메일 인증

--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -2,6 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import apiHandler, { ResponseType } from "@libs/backend/apiHandler";
 import client from "@libs/backend/client";
 import twilio from "twilio";
+import mail from "@sendgrid/mail";
+
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 const twilioClient = twilio(
   process.env.TWILIO_SID,
@@ -39,6 +42,14 @@ async function handler(
       body: `당신의 로그인 토큰은 ${randomNumber} 입니다.`
     });
     console.log(twilioResponse);
+  } else if (email) {
+    const emailResponse = await mail.send({
+      from: "aristataetc@gmail.com",
+      to: "aristataetc@gmail.com",
+      subject: "인증 메일 테스트",
+      text: `당신의 토큰은 ${randomNumber}`
+    });
+    console.log(emailResponse);
   }
   return res.status(200).json({ ok: true });
 }

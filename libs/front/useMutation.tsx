862ecd5fa0528a7bfeba955c05be4e7128f2a486
장성민 <aristatait@gmail.com>
2022-07-20 10:03:48 +0900

    Refactor: 결과 타입을 인터페이스와 타입으로 리팩토링
    useMutation 함수의 결과 타입을 인터페이스와 타입으로 빼서 코드를 깔끔하게 수정하였다

--- libs/front/useMutation.tsx
+++ libs/front/useMutation.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
-export default function useMutation(url: string): [
-  (data: any) => void,
-  {
-    loading: boolean;
-    data: undefined | any;
-    error: undefined | any;
-  }
-] {
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+
+type UseMutationResult = [(data: any) => void, UseMutationState];
+
+export default function useMutation(url: string): UseMutationResult {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);

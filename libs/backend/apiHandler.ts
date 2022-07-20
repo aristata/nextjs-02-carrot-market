ab8bb9fc1e2c1a38c7d4eb38e16f0bcfb756ac74
장성민 <aristatait@gmail.com>
2022-07-20 17:25:19 +0900

    Feat: SMS 보내기
    로그인시 생성된 토큰을 SMS 로 전송하기

--- libs/backend/apiHandler.ts
+++ libs/backend/apiHandler.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+export interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
+
 export default function apiHandler(
   method: "GET" | "POST" | "DELETE",
   fn: (req: NextApiRequest, res: NextApiResponse) => void
--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -1,10 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import apiHandler from "@libs/backend/apiHandler";
+import apiHandler, { ResponseType } from "@libs/backend/apiHandler";
 import client from "@libs/backend/client";
+import twilio from "twilio";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const userInfo = phone ? { phone: +phone } : { email };
+  const userInfo = phone ? { phone: +phone } : email ? { email } : null;
+  if (!userInfo) return res.status(400).json({ ok: false });
   const randomNumber = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,8 +32,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       }
     }
   });
-  console.log(token);
-  return res.status(200).end();
+  if (phone) {
+    const twilioResponse = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MS_SID,
+      to: process.env.MY_PHONE!,
+      body: `당신의 로그인 토큰은 ${randomNumber} 입니다.`
+    });
+    console.log(twilioResponse);
+  }
+  return res.status(200).json({ ok: true });
 }
 
 export default apiHandler("POST", handler);

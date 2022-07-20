228b0be2048a13efe156c6878820960c606fbd5d
장성민 <aristatait@gmail.com>
2022-07-20 14:04:32 +0900

    Feat: 유저 찾기 및 등록
    이메일 또는 폰 번호로 유저 정보를 찾고, 유저 정보가 없으면 등록을 하고, 유저 정보가 있으면 찾은 유저 정보를 반환한다

--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -3,7 +3,48 @@ import apiHandler from "@libs/backend/apiHandler";
 import client from "@libs/backend/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone
+      }
+    });
+    if (!user) {
+      console.log("유저를 찾을 수 없습니다. 새로 생성합니다.");
+      user = await client.user.create({
+        data: {
+          phone: +phone,
+          name: "Anonymous"
+        }
+      });
+    } else {
+      console.log("유저를 찾았습니다.");
+    }
+  }
+
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+    if (!user) {
+      console.log("유저를 찾을 수 없습니다. 새로 생성합니다.");
+      user = await client.user.create({
+        data: {
+          email: email,
+          name: "Anonymous"
+        }
+      });
+    } else {
+      console.log("유저를 찾았습니다.");
+    }
+  }
+
+  console.log(user);
   return res.status(200).end();
 }
 

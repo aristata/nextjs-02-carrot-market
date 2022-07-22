cf732ad62c2c61b0d9fc0bd550d8ac9d279eae0a
장성민 <aristatait@gmail.com>
2022-07-22 10:17:17 +0900

    Feat: Iron Session 적용

--- pages/api/user/confirm.tsx
+++ pages/api/user/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import apiHandler, { ResponseType } from "@libs/backend/apiHandler";
 import client from "@libs/backend/client";
@@ -6,9 +7,27 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  console.log(token);
+  const { token: inputToken } = req.body;
+  console.log(`inputToken = ${inputToken}`);
+
+  const outputToken = await client.token.findUnique({
+    where: {
+      payload: inputToken
+    },
+    include: {
+      user: true
+    }
+  });
+  if (!outputToken) res.status(404).end();
+
+  req.session.user = {
+    id: outputToken?.userId
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default apiHandler("POST", handler);
+export default withIronSessionApiRoute(apiHandler("POST", handler), {
+  cookieName: "carrotSession",
+  password: process.env.IRON_SESSION_PASSWORD!
+});

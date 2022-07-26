f2c1ca34547dc4d3f8b1d5bf996b0c8e4e6f5923
장성민 <aristatait@gmail.com>
2022-07-26 17:41:45 +0900

    Feat: SWR 적용

--- libs/frontend/useUser.ts
+++ libs/frontend/useUser.ts
@@ -1,18 +1,23 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
   const router = useRouter();
-  useEffect(() => {
-    fetch("/api/user/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.push("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+  const { data, error } = useSWR("/api/user/me", fetcher);
+  return data;
+  // const [user, setUser] = useState();
+  // useEffect(() => {
+  //   fetch("/api/user/me")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (!data.ok) {
+  //         return router.push("/enter");
+  //       }
+  //       setUser(data.profile);
+  //     });
+  // }, [router]);
+  // return user;
 }

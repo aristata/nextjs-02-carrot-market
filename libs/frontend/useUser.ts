f62e2bac705fc3bfdca6a711b7322603f1126e3a
장성민 <aristatait@gmail.com>
2022-07-28 10:59:24 +0900

    Feat: SWR 을 사용하여 user 조회

--- libs/frontend/useUser.ts
+++ libs/frontend/useUser.ts
@@ -7,7 +7,12 @@ const fetcher = (url: string) => fetch(url).then((response) => response.json());
 export default function useUser() {
   const router = useRouter();
   const { data, error } = useSWR("/api/user/me", fetcher);
-  return data;
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  return { user: data?.profile, isLoading: !data && !error };
   // const [user, setUser] = useState();
   // useEffect(() => {
   //   fetch("/api/user/me")

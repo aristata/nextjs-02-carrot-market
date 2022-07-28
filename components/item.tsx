d5c9daa477c0dfc42dc17c1f7c31c6a4894f62a4
장성민 <aristatait@gmail.com>
2022-07-28 11:13:20 +0900

    Rename: items 폴더 이름을 products 로 변경함

--- components/item.tsx
+++ components/item.tsx
@@ -13,13 +13,13 @@ export default function Item({
   price,
   comments,
   hearts,
-  id,
+  id
 }: ItemProps) {
   return (
-    <Link href={`/items/${id}`}>
+    <Link href={`/products/${id}`}>
       <a className="flex px-4 pt-5 cursor-pointer justify-between">
         <div className="flex space-x-4">
-          <div className="w-20 h-20 bg-gray-400 rounded-md"/>
+          <div className="w-20 h-20 bg-gray-400 rounded-md" />
           <div className="pt-2 flex flex-col">
             <h3 className="text-sm font-medium text-gray-900">{title}</h3>
             <span className="font-medium mt-1 text-gray-900">${price}</span>

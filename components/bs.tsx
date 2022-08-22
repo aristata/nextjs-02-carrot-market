554062cb17681be223f2df9e041bfb77ef2eaaad
장성민 <aristatait@gmail.com>
2022-08-22 17:22:52 +0900

    feat: dynamic import

---
+++ components/bs.tsx
@@ -0,0 +1,4 @@
+console.log("이건 다이나믹 임포트를 테스트하기 위한 코드입니다.");
+export default function Bs() {
+  return <h1>Hello</h1>;
+}
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -6,6 +6,10 @@ import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
 import { useRouter } from "next/router";
+// import Bs from "@components/bs";
+import dynamic from "next/dynamic";
+
+const Bs = dynamic(() => import("@components/bs"), { ssr: false });
 
 interface EnterForm {
   email?: string;
@@ -118,14 +122,17 @@ const Enter: NextPage = () => {
                 />
               ) : null}
               {method === "phone" ? (
-                <Input
-                  register={register("phone")}
-                  name="phone"
-                  label="Phone number"
-                  type="number"
-                  kind="phone"
-                  required
-                />
+                <>
+                  <Bs />
+                  <Input
+                    register={register("phone")}
+                    name="phone"
+                    label="Phone number"
+                    type="number"
+                    kind="phone"
+                    required
+                  />
+                </>
               ) : null}
               {method === "email" ? (
                 <Button loading={loading} text="Get login link" />

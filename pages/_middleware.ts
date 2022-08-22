7466d246a169633b45de823cfbf41ec0779d2929
장성민 <aristatait@gmail.com>
2022-08-22 16:24:08 +0900

    feat: bot check

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,3 +1,11 @@
-export function middleware() {
-  console.log("/pages/_middleware.ts 가 호출 되었습니다");
+import type { NextRequest, NextFetchEvent } from "next/server";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (req.ua?.isBot) {
+    console.log("/pages/_middleware.ts 가 호출 되었습니다");
+    return new Response(
+      "봇 사용이 감지되었습니다. 해당 요청을 완료할 수 없습니다. " +
+        "만약 봇이 아닌데 해당 에러가 표시된다면, 관리자에게 문의하세요.",
+      { status: 403 }
+    );
+  }
 }

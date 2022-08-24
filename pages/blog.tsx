ba70d1f5b37c39ff36f38d9dee541aa7756eb9c1
earnestgem <aristatait@gmail.com>
2022-08-24 17:05:00 +0900

    feat: getStaticProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,5 @@
 import Layout from "@components/layout";
+import { readdirSync } from "fs";
 
 const Blog = () => {
   return (
@@ -8,5 +9,17 @@ const Blog = () => {
     </Layout>
   );
 };
+
+/**
+ * getServersideProps() 는 페이지가 요청될 때마다 호출되어 실행된다
+ * 반면에 getStaticProps() 는 딱 한번, 이 페이지가 빌드되고 nextjs 가 이 페이지를 Html 로 생성할 때 딱 한번만 호출되어 실행된다
+ */
+export async function getStaticProps() {
+  // readdirSync 는 디렉토리의 파일들을 불러오는 nodejs 함수 이다
+  const files = readdirSync("./posts");
+  return {
+    props: {}
+  };
+}
 
 export default Blog;

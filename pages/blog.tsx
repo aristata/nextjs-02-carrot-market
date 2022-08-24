059d3bac5506aa119ae968c761dc9b960233bc56
earnestgem <aristatait@gmail.com>
2022-08-24 15:59:57 +0900

    feat: Blog

---
+++ pages/blog.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/layout";
+
+const Blog = () => {
+  return (
+    <Layout title="블로그">
+      <h1 className="font-semibold text-lg">Latest Posts:</h1>
+      <ul></ul>
+    </Layout>
+  );
+};
+
+export default Blog;
